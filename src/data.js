ffb0005a1d419878e6c142990e4f0856b3cd2658
Ilya Sidorchik <ilya@sidorchik.ru>
2020-09-19 18:55:31 +0300

    Move data to separate file

---
+++ src/data.js
@@ -0,0 +1,66 @@
+const subjectList = [
+  {
+    name: 'math',
+    title: 'Математика профильная',
+  },
+  {
+    name: 'mathb',
+    title: 'Математика базовая',
+  },
+  {
+    name: 'inf',
+    title: 'Информатика',
+  },
+  {
+    name: 'rus',
+    title: 'Русский язык',
+  },
+  {
+    name: 'en',
+    title: 'Английский язык',
+  },
+  {
+    name: 'de',
+    title: 'Немецкий язык',
+  },
+  {
+    name: 'fr',
+    title: 'Французcкий язык',
+  },
+  {
+    name: 'sp',
+    title: 'Испанский язык',
+  },
+  {
+    name: 'phys',
+    title: 'Физика',
+  },
+  {
+    name: 'chem',
+    title: 'Химия',
+  },
+  {
+    name: 'bio',
+    title: 'Биология',
+  },
+  {
+    name: 'geo',
+    title: 'География',
+  },
+  {
+    name: 'soc',
+    title: 'Обществознание',
+  },
+  {
+    name: 'lit',
+    title: 'Литература',
+  },
+  {
+    name: 'hist',
+    title: 'История',
+  },
+];
+
+module.exports = {
+  subjectList,
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const express = require(`express`);
+const express = require('express');
+
+const { subjectList } = require('./data');
 
 const PORT = process.env.PORT || 4000;
 
@@ -24,69 +26,6 @@ app.get('/api/search', (req, res) => {
   const { query } = req.query;
   if (!query) res.json(null);
   const subjectQuery = query.toLowerCase();
-
-  const subjectList = [
-    {
-      name: 'math',
-      title: 'Математика профильная',
-    },
-    {
-      name: 'mathb',
-      title: 'Математика базовая',
-    },
-    {
-      name: 'inf',
-      title: 'Информатика',
-    },
-    {
-      name: 'rus',
-      title: 'Русский язык',
-    },
-    {
-      name: 'en',
-      title: 'Английский язык',
-    },
-    {
-      name: 'de',
-      title: 'Немецкий язык',
-    },
-    {
-      name: 'fr',
-      title: 'Французcкий язык',
-    },
-    {
-      name: 'sp',
-      title: 'Испанский язык',
-    },
-    {
-      name: 'phys',
-      title: 'Физика',
-    },
-    {
-      name: 'chem',
-      title: 'Химия',
-    },
-    {
-      name: 'bio',
-      title: 'Биология',
-    },
-    {
-      name: 'geo',
-      title: 'География',
-    },
-    {
-      name: 'soc',
-      title: 'Обществознание',
-    },
-    {
-      name: 'lit',
-      title: 'Литература',
-    },
-    {
-      name: 'hist',
-      title: 'История',
-    },
-  ];
 
   let subject = null;
 
